1b963930cec24c2ed9d5225a8b3c923483c24a06
Ronaldo Mendonça Zica <ronaldomendoncazica@gmail.com>
2026-06-10 20:44:20 -0300

    fix: fix api path

--- api/index.js
+++ api/index.js
@@ -7,13 +7,9 @@ app.use(require("cors")());
 
 // ─── Redis Client (Upstash) ──────────────────────────────────────────────────
 function getKV() {
-  if (!process.env.UPSTASH_REDIS_REST_URL || !process.env.UPSTASH_REDIS_REST_TOKEN) {
-    return null;
-  }
-
   return new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
   });
 }
 
